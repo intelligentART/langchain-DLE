0903588171ccdc4904634994f1cd386556bcd323
intelligentART <37393998+intelligentART@users.noreply.github.com>
2023-07-07 11:07:53 -0700

    Update app.py

--- slack/app.py
+++ slack/app.py
@@ -5,7 +5,7 @@ from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, request
-from functions import draft_email
+from elaine import draft_email
 
 # Load environment variables from .env file
 load_dotenv(find_dotenv())
